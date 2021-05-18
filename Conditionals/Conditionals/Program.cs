27081743102bdd5817e98a2fa14b55fea997e0ca
“ilaydaakbulut” <akbulut.ilayda@yahoo.com.tr>
2021-05-18 20:14:58 +0300

    project added

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 11;
+            Console.WriteLine(number == 10 ? "number is 10" : "number is not 10" );
+            switch (number)
+            {
+                case 10:
+                    Console.WriteLine("number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("number is 20");
+                    break;
+                default:
+                    Console.WriteLine("number is not 10 or 20");
+                    break;
+            }
+        }
+    }
+}
