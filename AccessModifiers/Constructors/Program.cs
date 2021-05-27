fe61f506345257b22dc0e806d1b4a749bbb1f140
“ilaydaakbulut” <akbulut.ilayda@yahoo.com.tr>
2021-05-27 15:28:33 +0300

    constructors have been added

---
+++ AccessModifiers/Constructors/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(10);
+            customerManager.Add();
+
+            Product product = new Product
+            {
+                Id = 1,
+                Name = "Laptop"
+            };
+            Product product1 = new Product(2, "Computer");
+            EmployeeManager employeeManager = new EmployeeManager(new DatabaseLogger());
+            employeeManager.Add();
+
+            PersonManager personManager = new PersonManager("product");
+            personManager.Add();
+
+            Teacher.Number = 10;
+            Utilities.Validate();
+            Manager.DoSomething();
+            Manager manager = new Manager();
+            manager.DoSomething2();
+
+            Console.ReadLine();
+        }
+    }
+    class CustomerManager
+    {
+        private int _count;
+        public CustomerManager(int count)
+        {
+            _count = count;
+        }
+        public CustomerManager()
+        {
+
+        }
+        public void List()
+        {
+            Console.WriteLine("listed {0} items",_count);
+        }
+        public void Add()
+        {
+            Console.WriteLine("added {0} items", _count);
+        }
+    }
+    class Product
+    {
+        public Product() 
+        {
+
+        }
+        private int _id;
+        private string _name;
+        public Product(int id,string name)
+        {
+            _id = id;
+            _name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    interface ILogger
+    {
+        void Log();
+        
+    }
+    class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("logged to database");
+        }
+    }
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("logged to file");
+        }
+    }
+    class EmployeeManager
+    {
+        private ILogger _logger;
+        public EmployeeManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Add()
+        {
+            _logger.Log();
+            Console.WriteLine("Added");
+        }
+    }
+    class BaseClass
+    {
+        private string _entity;
+        public BaseClass(string entitiy)
+        {
+            _entity = entitiy;
+        }
+        public void Message()
+        {
+            Console.WriteLine("{0} message",_entity);
+        }
+    }
+    class PersonManager : BaseClass
+    {
+        public PersonManager(string entity):base(entity)
+        {
+
+        }
+        public void Add()
+        {
+            Console.WriteLine("added");
+            Message();
+        }
+
+    }
+    static class Teacher
+    {
+        public static int Number { get; set; }
+    }
+    static class Utilities
+    {
+        static Utilities()
+        {
+
+        }
+        public static void Validate()
+        {
+            Console.WriteLine("validation is done");
+        }
+    }
+    class Manager
+    {
+        public static void DoSomething()
+        {
+            Console.WriteLine("Done");
+        }
+        public void DoSomething2()
+        {
+            Console.WriteLine("Done 2");
+        }
+    }
+}
