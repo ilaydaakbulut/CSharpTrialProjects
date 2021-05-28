2d6bebca4f3840b24528e3f3febf840312092b27
“ilaydaakbulut” <akbulut.ilayda@yahoo.com.tr>
2021-05-28 09:47:50 +0300

    Collections have been added

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string[] cities = new string[2] { "Ankara", "istanbul" };
+            cities = new string[3];
+            cities[2] = "adana";
+            Console.WriteLine(cities[0]);
+            */
+
+            ArrayList cities = new ArrayList();
+            cities.Add("ankara");
+            cities.Add("adana");
+            cities.Add(1);
+            cities.Add('a');
+
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city);
+            }
+            Console.WriteLine("****************");
+
+            List<string> cities2 = new List<string>();
+            cities2.Add("ankara");
+            Console.WriteLine(cities2.Contains("ankara"));//true döner
+
+            foreach (var city in cities2)
+            {
+                Console.WriteLine(city);
+            }
+            Console.WriteLine("******************");
+            List<Customer> customers = new List<Customer>();
+            customers.Add(new Customer { Id = 1, FirstName = "ilayda" });
+            customers.Add(new Customer { Id = 2, FirstName = "melisa" });
+            /*
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer { Id = 1, FirstName = "ilayda" },
+                new Customer { Id = 2, FirstName = "melisa" }
+            };
+            */
+            var customer2 = new Customer
+            {
+                Id = 3,
+                FirstName = "ecem"
+            };
+            customers.Add(customer2);
+            customers.AddRange(new Customer[2]
+            {
+                new Customer{ Id=4,FirstName="ali"},
+                new Customer{Id=5,FirstName="ayse"}
+            });
+            //customers.Clear();//listenin elemanlarını siler.
+
+            var index = customers.IndexOf(customer2); 
+            Console.WriteLine("Index : {0} ",index);
+            customers.Add(customer2);
+            var index2= customers.LastIndexOf(customer2);
+            Console.WriteLine("Index2 : {0} ", index2);
+
+            customers.Insert(0,customer2);//istenen indexse sayı eklenir.
+            customers.Remove(customer2);
+            customers.Remove(customer2);
+            customers.RemoveAll(c => c.FirstName == "ilayda");
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+            var count = customers.Count;
+            Console.WriteLine(count);
+            Console.WriteLine("******************");
+
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("book", "kitap");
+            dictionary.Add("table", "masa");
+            dictionary.Add("computer", "bilgisayar");
+
+            Console.WriteLine(dictionary["table"] );
+            //Console.WriteLine(dictionary["glass"] ); 
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Key+"  "+item.Value);
+            }
+            Console.WriteLine(dictionary.ContainsKey("glass")  ); //false döner 
+            Console.ReadLine();
+        }
+    }
+    class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+    }
+}
---
+++ ReferanceAndValueTyoe/ReferanceAndValueTyoe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferanceAndValueTyoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 20;
+            number2 = number1;
+            number1 = 30;
+
+            Console.WriteLine(number2);
+            Console.WriteLine(number1);
+
+
+            string[] cities1 = new string[] { "ankara", "adana", "afyon" };
+            string[] cities2 = new string[] { "bursa", "balıkesir", "bolu" };
+            cities2 = cities1;
+            Console.WriteLine(cities2[0]);
+            cities2[0] = "istanbul";
+            Console.WriteLine(cities2[0]);
+
+            DataTable datatable ;
+            DataTable datatable2 = new DataTable();
+            datatable = datatable2;
+            
+            Console.ReadLine();
+        }
+    }
+}
